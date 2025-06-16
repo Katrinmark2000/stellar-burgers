d3605615b7bf322920805ba5f0730f2e2f7976c5
Mkkkash <marckovakatrin@yandex.ru>
2025-06-17 01:56:18 +0300

    fix tests

--- src/__tests__/rootReduce.test.ts
+++ src/__tests__/rootReduce.test.ts
@@ -1,50 +1,59 @@
 import store from '../services/store';
-import { RootState } from '../services/store';
-
+import { RootState, rootReducer } from '../services/store';
 
 describe('rootReducer', () => {
-    it('должен инициализировать состояние корректно', () => {
-
+  it('должен инициализировать состояние корректно', () => {
     const state: RootState = store.getState();
 
     expect(state.ingredients).toEqual({
+      ingredients: [],
+      error: null,
+      loading: false
+    });
+
+    expect(state.ordersUser).toEqual({
+      orders: [],
+      error: null,
+      loading: false
+    });
+
+    expect(state.user).toEqual({
+      user: null,
+      isAuthChecked: false,
+      errorUser: undefined
+    });
+
+    expect(state.feed).toEqual({
+      orders: [],
+      total: 0,
+      error: null,
+      loading: false,
+      orderNumber: null,
+      totalToday: 0
+    });
+
+    expect(state.constructorBurger).toEqual({
+      constructorItems: {
         ingredients: [],
-        error: null,
-        loading: false
-      });
+        bun: null
+      },
+      orderRequest: false,
+      orderModalData: null,
+      error: null,
+      loading: false
+    });
+  });
 
-      expect(state.ordersUser).toEqual({
-        orders: [],
-        error: null,
-        loading: false
-      });
-      
-      expect(state.user).toEqual({
-        user: null,
-        isAuthChecked: false,
-        errorUser: undefined
-      });
+  it('не мутирует состояние при вызове с неизвестным экшеном', () => {
+    const currentState = store.getState();
 
-      expect(state.feed).toEqual({
-        orders: [],
-        total: 0,
-        error: null,
-        loading: false,
-        orderNumber: null,
-        totalToday: 0
-      });
+    //создаем фиктивный неизвестный экшен
+    const unknownAction = { type: 'UNKNOWN_ACTION' };
 
-      expect(state.constructorBurger).toEqual({
-        constructorItems: {
-          ingredients: [],
-          bun: null
-        },
-        orderRequest: false,
-        orderModalData: null,
-        error: null,
-        loading: false
-      });
-    }
-    )
-}
-)
+    //вызываем редьюсер напрямую с текущим состоянием и неизвестным экшеном
+    const newState = rootReducer(currentState, unknownAction);
+
+    //проверяем, что состояние не изменилось (сравниваем по ссылке)
+    expect(newState).toBe(currentState);
+  });
+});
